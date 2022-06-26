82e9c5f67d028ba5e0efee1c1c5497ef3df608ae
ludswdy03 <j031024y@naver.com>
2022-06-26 12:14:59 +0900

    '5100_period_code'

---
+++ bus_period_5100.py
@@ -0,0 +1,43 @@
+import requests
+import schedule
+import time
+import pandas as pd
+
+# 시간 정보 저장할 리스트 생성
+bus_time = []
+
+def find_period():
+    # api 사용 url
+    url = "http://apis.data.go.kr/6410000/busarrivalservice/getBusArrivalItem"
+
+    bio = {"serviceKey" : "bwUUK6MEA6GnqDpo2ZS9ChN4VZ1nkUPXrryU80XxUcLqxB62pIGIPWXNdyzqgT20BHrw8zxWWk9rqiSQwTrbnQ==",
+                "stationId" : "228000704", "routeId" : "200000115"}
+
+    bio_res = requests.get(url, params=bio)
+
+    # xml문서 string으로 변환
+    station = str(bio_res.content, "utf-8")
+
+
+    #버스 정보 존재 시 시간 입력
+    if "정상적으로 처리되었습니다" in station:
+        bus_time.append(time.ctime())
+
+# 1초마다 반복
+schedule.every(1).seconds.do(find_period)
+
+count = 0
+# 900초 동안 반복 실행
+while count < 2000:
+    schedule.run_pending()
+    time.sleep(1)
+    # 퍼센트로 진행률 출력
+    print("진행중..", round((count + 1) / 20, 2), "%")
+    count = count + 1
+
+# 엑셀파일로 데이터 저장
+bus_time_data = {"time" : bus_time}
+
+bus_time_data = pd.DataFrame(bus_time_data)
+bus_time_data.to_excel(excel_writer="/Users/yang-jinyoung/Desktop/동아리 대항전/period_5100_3.xlsx")
+print("완료")
