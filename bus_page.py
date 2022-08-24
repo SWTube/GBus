cce037861319a447dc1c9c2469f70ffd97fa3e4f
ludswdy03 <j031024y@naver.com>
2022-08-25 03:19:27 +0900

    final_khubus

--- bus_page.py
+++ bus_page.py
@@ -2,17 +2,28 @@ from flask import Flask, render_template
 from getInput import *
 from timeconvert import *
 import pickle
+from pytz import timezone
 app = Flask(__name__)
+
+
+utc_now = datetime.datetime.utcnow()
+kr_time = datetime.datetime.now(timezone('Asia/Seoul'))
+
+nowtime = convertToStringTime(kr_time.hour, kr_time.minute)
+
+def krtimeToMin():
+    minute = (int(kr_time.hour) * 60) + int(kr_time.minute)
+    return minute
 
 @app.route('/')
 def index():
     info_message = "정류장을 클릭하여 버스 도착 정보를 확인하세요."
-    return render_template("buspage/bus_main.html", info_message=info_message)
+    return render_template("buspage/bus_main.html", info_message=info_message, now_time=nowtime)
 
 @app.route('/busstop/<id>/')
 def bus_stop(id):
     info_message = f"{id} 정류장 버스 도착 정보입니다."
-    return render_template("bus_info.html", info_message=info_message)
+    return render_template("bus_info.html", info_message=info_message, now_time=nowtime)
 
 
 @app.route('/busstop/사색의광장/')
@@ -27,10 +38,10 @@ def indexa():
         inputVal=trim_data(data,7000)
         result7 = model.predict(inputVal)
     except requests.Timeout as err:
-        result7='errorRequestsTimeout'
+        result7=False
     except Exception as e:
-        result7='errorException'
-    
+        result7=False
+
     model = pickle.load(open('5100_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p5100)
@@ -41,11 +52,11 @@ def indexa():
         inputVal=trim_data(data,5100)
         result5 = model.predict(inputVal)
     except requests.Timeout as err:
-        result5='errorRequestsTimeout'
+        result5=False
     except Exception as e:
-        result5='errorException'
-    
-    
+        result5=False
+
+
     model = pickle.load(open('9_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p0009)
@@ -56,11 +67,11 @@ def indexa():
         inputVal=trim_data(data,9)
         result0 = model.predict(inputVal)
     except requests.Timeout as err:
-        result0='errorRequestsTimeout'
+        result0=False
     except Exception as e:
-        result0='errorException'
+        result0=False
 
-    
+
     model = pickle.load(open('1112_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p1112)
@@ -71,11 +82,11 @@ def indexa():
         inputVal=trim_data(data,1112)
         result1 = model.predict(inputVal)
     except requests.Timeout as err:
-        result1='errorRequestsTimeout'
+        result1=False
     except Exception as e:
-        result1='errorException'
-    
-    
+        result1=False
+
+
     model = pickle.load(open('7000_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p7000)
@@ -86,15 +97,40 @@ def indexa():
         inputVal=trim_data(data,7000)
         result7 = model.predict(inputVal)
     except requests.Timeout as err:
-        result7='errorRequestsTimeout'
+        result7=False
     except Exception as e:
-        result7='errorException'
-    return render_template('buspage/bus_info/bus_infosa.html', info_message='사색의 광장 정류장 도착시간 입니다.',
-    infoTime7=convertToStringTime(convertedHour(result7),convertedMinute(result7)),
-    infoTime5=convertToStringTime(convertedHour(result5),convertedMinute(result5)),
-    infoTime1=convertToStringTime(convertedHour(result1),convertedMinute(result1)),
-    infoTime0=convertToStringTime(convertedHour(result0),convertedMinute(result0))
-    )
+        result7=False
+    now_min = krtimeToMin()
+
+    if(result7==False or result0==False or result5==False or result1==False):
+        return render_template('buspage/bus_info/bus_except.html',info_message='다시 정류장을 클릭하세요',nowtime = convertToStringTime(kr_time.hour, kr_time.minute),
+        infoTime7="에러 발생",
+        infoTime5="에러 발생",
+        infoTime1="에러 발생",
+        infoTime0="에러 발생")
+    else:
+        if (10 < now_min < 330):
+            infoTime5 = "버스 없음"
+        else:
+            infoTime5 = convertToStringTime(convertedHour(result5,utc_now),convertedMinute(result5,utc_now)) + "<br>도착예정"
+        if (279 < now_min < 1351):
+            infoTime1 = convertToStringTime(convertedHour(result7,utc_now),convertedMinute(result7,utc_now)) + "<br>도착예정"
+        else:
+            infoTime1 = "버스 없음"
+        if (0 < now_min < 330):
+            infoTime7 = "버스 없음"
+        else:
+            infoTime7 = convertToStringTime(convertedHour(result7,utc_now),convertedMinute(result7,utc_now)) + "<br>도착예정"
+        if (329 < now_min < 1381):
+            infoTime0 = convertToStringTime(convertedHour(result0,utc_now),convertedMinute(result0,utc_now)) + "<br>도착예정"
+        else:
+            infoTime0 = "버스 없음"
+        return render_template('buspage/bus_info/bus_info.html', info_message='사색의 광장 정류장 도착시간 입니다.',nowtime = convertToStringTime(kr_time.hour, kr_time.minute),
+        infoTime7=infoTime7,
+        infoTime5=infoTime5,
+        infoTime1=infoTime1,
+        infoTime0=infoTime0
+        )
 
 
 
@@ -110,10 +146,10 @@ def indexaa():
         inputVal=trim_data(data,7000)
         result7 = model.predict(inputVal)
     except requests.Timeout as err:
-        result7='errorRequestsTimeout'
+        result7=False
     except Exception as e:
-        result7='errorException'
-    
+        result7=False
+
     model = pickle.load(open('5100_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p5100)
@@ -124,11 +160,11 @@ def indexaa():
         inputVal=trim_data(data,5100)
         result5 = model.predict(inputVal)
     except requests.Timeout as err:
-        result5='errorRequestsTimeout'
+        result5=False
     except Exception as e:
-        result5='errorException'
-    
-    
+        result5=False
+
+
     model = pickle.load(open('9_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p0009)
@@ -139,11 +175,11 @@ def indexaa():
         inputVal=trim_data(data,9)
         result0 = model.predict(inputVal)
     except requests.Timeout as err:
-        result0='errorRequestsTimeout'
+        result0=False
     except Exception as e:
-        result0='errorException'
+        result0=False
 
-    
+
     model = pickle.load(open('1112_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p1112)
@@ -154,11 +190,11 @@ def indexaa():
         inputVal=trim_data(data,1112)
         result1 = model.predict(inputVal)
     except requests.Timeout as err:
-        result1='errorRequestsTimeout'
+        result1=False
     except Exception as e:
-        result1='errorException'
-    
-    
+        result1=False
+
+
     model = pickle.load(open('7000_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p7000)
@@ -168,17 +204,42 @@ def indexaa():
         data=pd.DataFrame(pBus.bus_time,columns=['시간','운행중인 다른 버스들의 위치'])
         inputVal=trim_data(data,7000)
         result7 = model.predict(inputVal)
-        
+
     except requests.Timeout as err:
-        result7='errorRequestsTimeout'
+        result7=False
     except Exception as e:
-        result7='errorException'
-    return render_template('buspage/bus_info/bus_infope.html', info_message='체육대학외대 정류장 도착시간 입니다.',
-    infoTime7=convertToStringTime(convertedHour2(result7),convertedMinute2(result7)),
-    infoTime5=convertToStringTime(convertedHour2(result5),convertedMinute2(result5)),
-    infoTime1=convertToStringTime(convertedHour2(result1),convertedMinute2(result1)),
-    infoTime0=convertToStringTime(convertedHour2(result0),convertedMinute2(result0))
-    )
+        result7=False
+    now_min = krtimeToMin()
+
+    if(result7==False or result0==False or result5==False or result1==False):
+        return render_template('buspage/bus_info/bus_except.html',info_message='다시 정류장을 클릭하세요',nowtime = convertToStringTime(kr_time.hour, kr_time.minute),
+        infoTime7="에러 발생",
+        infoTime5="에러 발생",
+        infoTime1="에러 발생",
+        infoTime0="에러 발생")
+    else:
+        if (10 < now_min < 330):
+            infoTime5 = "버스 없음"
+        else:
+            infoTime5 = convertToStringTime(convertedHour(result5,utc_now),convertedMinute(result5,utc_now)) + "<br>도착예정"
+        if (279 < now_min < 1351):
+            infoTime1 = convertToStringTime(convertedHour(result7,utc_now),convertedMinute(result7,utc_now)) + "<br>도착예정"
+        else:
+            infoTime1 = "버스 없음"
+        if (0 < now_min < 330):
+            infoTime7 = "버스 없음"
+        else:
+            infoTime7 = convertToStringTime(convertedHour(result7,utc_now),convertedMinute(result7,utc_now)) + "<br>도착예정"
+        if (329 < now_min < 1381):
+            infoTime0 = convertToStringTime(convertedHour(result0,utc_now),convertedMinute(result0,utc_now)) + "<br>도착예정"
+        else:
+            infoTime0 = "버스 없음"
+        return render_template('buspage/bus_info/bus_info.html', info_message='체육대학외대 정류장 도착시간 입니다.',nowtime = convertToStringTime(kr_time.hour, kr_time.minute),
+        infoTime7=infoTime7,
+        infoTime5=infoTime5,
+        infoTime1=infoTime1,
+        infoTime0=infoTime0
+        )
 
 
 @app.route('/busstop/생명과학대/')
@@ -193,10 +254,10 @@ def indexaaa():
         inputVal=trim_data(data,7000)
         result7 = model.predict(inputVal)
     except requests.Timeout as err:
-        result7='errorRequestsTimeout'
+        result7=False
     except Exception as e:
-        result7='errorException'
-    
+        result7=False
+
     model = pickle.load(open('5100_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p5100)
@@ -207,11 +268,11 @@ def indexaaa():
         inputVal=trim_data(data,5100)
         result5 = model.predict(inputVal)
     except requests.Timeout as err:
-        result5='errorRequestsTimeout'
+        result5=False
     except Exception as e:
-        result5='errorException'
-    
-    
+        result5=False
+
+
     model = pickle.load(open('9_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p0009)
@@ -222,11 +283,11 @@ def indexaaa():
         inputVal=trim_data(data,9)
         result0 = model.predict(inputVal)
     except requests.Timeout as err:
-        result0='errorRequestsTimeout'
+        result0=False
     except Exception as e:
-        result0='errorException'
+        result0=False
 
-    
+
     model = pickle.load(open('1112_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p1112)
@@ -237,11 +298,11 @@ def indexaaa():
         inputVal=trim_data(data,1112)
         result1 = model.predict(inputVal)
     except requests.Timeout as err:
-        result1='errorRequestsTimeout'
+        result1=False
     except Exception as e:
-        result1='errorException'
-    
-    
+        result1=False
+
+
     model = pickle.load(open('7000_regressor.pkl', 'rb'))
     # 인풋값 생성
     pBus=Bus(p7000)
@@ -252,16 +313,40 @@ def indexaaa():
         inputVal=trim_data(data,7000)
         result7 = model.predict(inputVal)
     except requests.Timeout as err:
-        result7='errorRequestsTimeout'
+        result7=False
     except Exception as e:
-        result7='errorException'
-    return render_template('buspage/bus_info/bus_infobi.html', info_message='생명과학대 정류장 도착시간 입니다.',
-    infoTime7=convertToStringTime(convertedHour1(result7),convertedMinute1(result7)),
-    infoTime5=convertToStringTime(convertedHour1(result5),convertedMinute1(result5)),
-    infoTime1=convertToStringTime(convertedHour1(result1),convertedMinute1(result1)),
-    infoTime0=convertToStringTime(convertedHour1(result0),convertedMinute1(result0))
-    )
+        result7=False
+    now_min = krtimeToMin()
 
+    if(result7==False or result0==False or result5==False or result1==False):
+        return render_template('buspage/bus_info/bus_except.html',info_message='다시 정류장을 클릭하세요',nowtime = convertToStringTime(kr_time.hour, kr_time.minute),
+        infoTime7="에러 발생",
+        infoTime5="에러 발생",
+        infoTime1="에러 발생",
+        infoTime0="에러 발생")
+    else:
+        if (10 < now_min < 330):
+            infoTime5 = "버스 없음"
+        else:
+            infoTime5 = convertToStringTime(convertedHour(result5,utc_now),convertedMinute(result5,utc_now)) + "<br>도착예정"
+        if (279 < now_min < 1351):
+            infoTime1 = convertToStringTime(convertedHour(result7,utc_now),convertedMinute(result7,utc_now)) + "<br>도착예정"
+        else:
+            infoTime1 = "버스 없음"
+        if (0 < now_min < 330):
+            infoTime7 = "버스 없음"
+        else:
+            infoTime7 = convertToStringTime(convertedHour(result7,utc_now),convertedMinute(result7,utc_now)) + "<br>도착예정"
+        if (329 < now_min < 1381):
+            infoTime0 = convertToStringTime(convertedHour(result0,utc_now),convertedMinute(result0,utc_now)) + "<br>도착예정"
+        else:
+            infoTime0 = "버스 없음"
+        return render_template('buspage/bus_info/bus_info.html', info_message='생명과학대학 정류장 도착시간 입니다.',nowtime = convertToStringTime(kr_time.hour, kr_time.minute),
+        infoTime7=infoTime7,
+        infoTime5=infoTime5,
+        infoTime1=infoTime1,
+        infoTime0=infoTime0
+        )
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- timeconvert.py
+++ timeconvert.py
@@ -1,34 +1,44 @@
-import time
+import datetime
+import pytz
 
-def convertedHour(infoBusNum,currentTime=time.time()): #몇시 도착예정인지
-    arriveSec=currentTime+int(infoBusNum)
-    arriveHour = time.strftime("%H",time.localtime(arriveSec))
-    return arriveHour
-    
-def convertedMinute(infoBusNum,currentTime=time.time()):#몇분 도착예정인지
-    arriveSec = currentTime+int(infoBusNum)
-    arriveMin = time.strftime("%M",time.localtime(arriveSec))
-    return arriveMin
+kr = pytz.timezone('Asia/Seoul')
 
-def convertedHour1(infoBusNum,currentTime=time.time()): #몇시 도착예정인지
-    arriveSec=currentTime+int(infoBusNum)+90
-    arriveHour = time.strftime("%H",time.localtime(arriveSec))
-    return arriveHour
-    
-def convertedMinute1(infoBusNum,currentTime=time.time()):#몇분 도착예정인지
-    arriveSec = currentTime+int(infoBusNum)+90
-    arriveMin = time.strftime("%M",time.localtime(arriveSec))
-    return arriveMin
 
-def convertedHour2(infoBusNum,currentTime=time.time()): #몇시 도착예정인지
-    arriveSec=currentTime+int(infoBusNum)+120
-    arriveHour = time.strftime("%H",time.localtime(arriveSec))
-    return arriveHour
-    
-def convertedMinute2(infoBusNum,currentTime=time.time()):#몇분 도착예정인지
-    arriveSec = currentTime+int(infoBusNum)+120
-    arriveMin = time.strftime("%M",time.localtime(arriveSec))
-    return arriveMin
+def convertedHour(infoBusNum,currentTime): #몇시 도착예정인지
+    arriveSec = currentTime+ datetime.timedelta(seconds=int(infoBusNum))
+    kr_time = kr.normalize(arriveSec.astimezone(kr))
+    hour = kr_time.hour
+    return hour
+
+def convertedMinute(infoBusNum,currentTime):#몇분 도착예정인지
+    arriveSec = currentTime+ datetime.timedelta(seconds=int(infoBusNum))
+    kr_time = kr.normalize(arriveSec.astimezone(kr))
+    minute = kr_time.minute
+    return minute
+
+def convertedHour1(infoBusNum,currentTime): #몇시 도착예정인지
+    arriveSec = currentTime+ datetime.timedelta(seconds=(int(infoBusNum)+90))
+    kr_time = kr.normalize(arriveSec.astimezone(kr))
+    hour = kr_time.hour
+    return hour
+
+def convertedMinute1(infoBusNum,currentTime):#몇분 도착예정인지
+    arriveSec = currentTime+ datetime.timedelta(seconds=(int(infoBusNum)+90))
+    kr_time = kr.normalize(arriveSec.astimezone(kr))
+    minute = kr_time.minute
+    return minute
+
+def convertedHour2(infoBusNum,currentTime): #몇시 도착예정인지
+    arriveSec = currentTime+ datetime.timedelta(seconds=(int(infoBusNum)+120))
+    kr_time = kr.normalize(arriveSec.astimezone(kr))
+    hour = kr_time.hour
+    return hour
+
+def convertedMinute2(infoBusNum,currentTime):#몇분 도착예정인지
+    arriveSec = currentTime+ datetime.timedelta(seconds=(int(infoBusNum)+120))
+    kr_time = kr.normalize(arriveSec.astimezone(kr))
+    minute = kr_time.minute
+    return minute
 
 def convertToStringTime(hour,min):
     stringTime = "{0}시{1}분".format(int(hour),int(min))
