de9b63b89c9e84f0ab912b2ca6d3501b15931d81
Leejeongwoo <juny1alex@gmail.com>
2022-08-25 13:07:42 +0900

    localfinal

--- flaskproject/app/views/bus_page.py
+++ flaskproject/app/views/bus_page.py
@@ -10,7 +10,7 @@ bp = Blueprint('main',__name__,url_prefix='/')
 @bp.route('/')
 def index():
     info_message = "정류장을 클릭하여 버스 도착 정보를 확인하세요."
-    return render_template("buspage/bus_main.html", info_message=info_message,nowtime=time.strftime("%H시%M분",time.localtime(time.time())))
+    return render_template("buspage/bus_main.html", info_message=info_message)
 
 
 
@@ -89,13 +89,13 @@ def sa():
     except Exception as e:
         result7=False
     if(result7==False or result0==False or result5==False or result1==False):
-        return render_template('buspage/bus_info/bus_error.html',info_message='다시 정류장을 클릭하세요',nowtime=time.strftime("%H시%M분",time.localtime(time.time())),
+        return render_template('buspage/bus_info/bus_error.html',info_message='다시 정류장을 클릭하세요',
         infoTime7="에러 발생",
         infoTime5="에러 발생",
         infoTime1="에러 발생",
         infoTime0="에러 발생")
     else:
-        return render_template('buspage/bus_info/bus_info.html', info_message='사색의 광장 정류장 도착시간 입니다.',nowtime=time.strftime("%H시%M분",time.localtime(time.time())),
+        return render_template('buspage/bus_info/bus_info.html', info_message='사색의 광장 정류장 도착시간 입니다.',
         infoTime7=convertToStringTime(convertedHour(result7,time.time()),convertedMinute(result7,time.time())),
         infoTime5=convertToStringTime(convertedHour(result5,time.time()),convertedMinute(result5,time.time())),
         infoTime1=convertToStringTime(convertedHour(result1,time.time()),convertedMinute(result1,time.time())),
@@ -180,13 +180,13 @@ def pe():
     except Exception as e:
         result7=False
     if(result7==False or result0==False or result5==False or result1==False):
-        return render_template('buspage/bus_info/bus_error.html',info_message='다시 정류장을 클릭하세요',nowtime=time.strftime("%H시%M분",time.localtime(time.time())),
+        return render_template('buspage/bus_info/bus_error.html',info_message='다시 정류장을 클릭하세요',
         infoTime7="에러 발생",
         infoTime5="에러 발생",
         infoTime1="에러 발생",
         infoTime0="에러 발생")
     else:
-        return render_template('buspage/bus_info/bus_info.html', info_message='체육대학외대 정류장 도착시간 입니다.',nowtime=time.strftime("%H시%M분",time.localtime(time.time())),
+        return render_template('buspage/bus_info/bus_info.html', info_message='체육대학외대 정류장 도착시간 입니다.',
         infoTime7=convertToStringTime(convertedHour2(result7,time.time()),convertedMinute2(result7,time.time())),
         infoTime5=convertToStringTime(convertedHour2(result5,time.time()),convertedMinute2(result5,time.time())),
         infoTime1=convertToStringTime(convertedHour2(result1,time.time()),convertedMinute2(result1,time.time())),
@@ -268,13 +268,13 @@ def bio():
     except Exception as e:
         result7=False
     if(result7==False or result0==False or result5==False or result1==False):
-        return render_template('buspage/bus_info/bus_error.html',info_message='다시 정류장을 클릭하세요',nowtime=time.strftime("%H시%M분",time.localtime(time.time())),
+        return render_template('buspage/bus_info/bus_error.html',info_message='다시 정류장을 클릭하세요',
         infoTime7="에러 발생",
         infoTime5="에러 발생",
         infoTime1="에러 발생",
         infoTime0="에러 발생")
     else:
-        return render_template('buspage/bus_info/bus_info.html', info_message='생명과학대학 정류장 도착시간 입니다.',nowtime=time.strftime("%H시%M분",time.localtime(time.time())),
+        return render_template('buspage/bus_info/bus_info.html', info_message='생명과학대학 정류장 도착시간 입니다.',
         infoTime7=convertToStringTime(convertedHour1(result7,time.time()),convertedMinute1(result7,time.time())),
         infoTime5=convertToStringTime(convertedHour1(result5,time.time()),convertedMinute1(result5,time.time())),
         infoTime1=convertToStringTime(convertedHour1(result1,time.time()),convertedMinute1(result1,time.time())),
